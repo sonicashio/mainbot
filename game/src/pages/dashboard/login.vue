01474d4e496c92f0797fff8c2c411037e3a66ff1
Pantherai <pantherai.app@gmail.com>
2024-10-10 02:58:28 +0300

    Revert

--- game/src/pages/dashboard/login.vue
+++ game/src/pages/dashboard/login.vue
@@ -17,7 +17,7 @@ if (import.meta.dev) {
     await handleUserAuth(user);
 }
 
-async function handleUserAuth(user: !LoginWidgetUser): Promise<void> {
+async function handleUserAuth(user: LoginWidgetUser): Promise<void> {
     const params = new URLSearchParams();
     for (const key in user) {
         params.set(key, (user as { [key: string]: string })[key]);
@@ -50,6 +50,7 @@ async function handleUserAuth(user: !LoginWidgetUser): Promise<void> {
       <LoginWidget
         bot-username="sonicashgame_bot"
         :user-photo="true"
+        @auth="handleUserAuth"
       />
 
       {{ error }}
